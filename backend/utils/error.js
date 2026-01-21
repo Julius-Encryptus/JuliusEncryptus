4dfddf1f03dddb94970fa1ce277faba39f95a879
Nikunj Chauhan <nkca122@gmail.com>
2026-01-22 02:52:34 +0530

    init

--- backend/utils/error.js
+++
@@ -1,9 +0,0 @@
-module.exports = class ExpressError extends Error {
-  constructor(msg, code, status) {
-    super();
-    this.status = status;
-    this.code = code;
-    this.msg = msg;
-  }
-}
-
--- frontend/src/assets/js/error.js
+++
@@ -1,8 +0,0 @@
-export  class ExpressError extends Error {
-    constructor(msg, code, status) {
-      super();
-      this.status = status;
-      this.code = code;
-      this.msg = msg;
-    }
-  }
